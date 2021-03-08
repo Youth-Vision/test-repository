3577a36ccb432f8ca37757f8aea95a5885396ebb
youth-Vision <sortodanielsosa@gmail.com>
2021-03-08 00:24:08 -0600

    2do commit; Homepage Añadida

---
+++ Home-Page-/main.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "215px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("div-header").style.backgroundColor = "rgba(0,0,0,.0)"
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+    document.getElementById("div-header").style.backgroundColor = "rgba(0,0,0,.2)"
+}
